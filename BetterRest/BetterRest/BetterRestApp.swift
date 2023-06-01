be3d6f20d6fd9226b5edb8227374a4a0786ece81
Juliana Nielson <nielsonjuliana@gmail.com>
2023-06-01 09:43:23 -0600

    Days 26-28

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Juliana Nielson on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
